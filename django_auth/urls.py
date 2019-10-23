8e8d22a7017f8c348d087a927c4c94b24d4b5768
wicked777 <wicky.akloe@gmail.com>
2019-10-23 15:42:28 +0200

    Add template and logout functionality

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from accounts import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name="index"),
+    url(r'^accounts/logout$', views.logout, name="logout")
 ]
