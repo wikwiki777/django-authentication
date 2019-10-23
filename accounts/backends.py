0bab822539cdfb4d47b1629a9b65508ac2d4a77e
wicked777 <wicky.akloe@gmail.com>
2019-10-23 17:57:55 +0200

    Custom authentication with django backend

---
+++ accounts/backends.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+
+
+class EmailAuth:
+    """Authenticate a user by an exact match on the email and password"""
+
+    def authenticate(self, username=None, password=None):
+        """Get instance of 'User' based off the mail and verify the password"""
+
+        try:
+            user = User.objects.get(email=username)
+
+            if user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExit:
+            return None
+
+
+    def get_user(self, user_id):
+        """
+        Used by the Django Authentication system to retrieve
+        a user instance
+        """
+
+        try:
+            user = User.objects.get(pk=user_id)
+
+            if user.is_active:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
