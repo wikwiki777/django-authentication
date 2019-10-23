5c196ee75d8a735565d4cbc1a182387d90c8e67b
wicked777 <wicky.akloe@gmail.com>
2019-10-23 15:47:39 +0200

    Add message from back to front end

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Used for messages
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
